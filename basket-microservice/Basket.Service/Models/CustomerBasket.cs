323728ef6ba62b1e37eabf0780c746d566c8c7a2
NeshGogo <rafaelaguero10@hotmail.com>
2024-10-21 22:01:09 -0400

    Basket models

---
+++ basket-microservice/Basket.Service/Models/CustomerBasket.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.Service.Models;
+
+internal class CustomerBasket
+{
+    private readonly HashSet<BasketProduct> _products = [];
+    public IEnumerable<BasketProduct> Products => _products;
+    public required string CustomerId { get; init; }
+
+
+    public void AddBasketProduct(BasketProduct product)
+    {
+        var existingProduct = _products.FirstOrDefault(p => p.ProductId.Equals(product.ProductId));
+
+        if (existingProduct is null)
+        {
+            _products.Add(product);
+        }
+        else
+        {
+            _products.Remove(existingProduct);
+            _products.Add(product);
+        }
+    }
+
+    public void RemoveBasketProduct(string productId) => _products.RemoveWhere(p => p.ProductId == productId);
+}
