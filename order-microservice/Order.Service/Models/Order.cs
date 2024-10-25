5b11980a0372d6854e854ceb6e11b6c92322768c
NeshGogo <rafaelaguero10@hotmail.com>
2024-10-24 21:57:26 -0400

    Defing order models

---
+++ order-microservice/Order.Service/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿namespace Order.Service.Models;
+
+internal class Order
+{
+    private readonly List<OrderProduct> _orderProducts = [];
+    public IReadOnlyCollection<OrderProduct> OrderProducts => _orderProducts.AsReadOnly();
+    public required string CustomerId { get; init; }
+
+    public Guid OrderId { get; private set; }
+    public DateTime OrderDate { get; private set; }
+
+    public Order(Guid orderId, DateTime orderDate)
+    {
+        OrderDate = orderDate;
+        OrderId = orderId;
+    }
+
+    public void AddOrderProduct(string productId, int quantity)
+    {
+        var existingOrderProduct = _orderProducts.SingleOrDefault(p => p.ProductId == productId);
+
+        if (existingOrderProduct != null)
+        {
+            existingOrderProduct.AddQuantity(quantity);
+        }
+        else
+        {
+            var orderProduct = new OrderProduct { ProductId = productId };
+            orderProduct.AddQuantity(quantity);
+            _orderProducts.Add(orderProduct);
+        }
+    }
+}
