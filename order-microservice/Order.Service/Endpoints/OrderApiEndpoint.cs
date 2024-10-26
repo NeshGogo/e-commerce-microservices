963f0d8ad6c691dd270af10b9e1ab55ce8694b69
NeshGogo <rafaelaguero10@hotmail.com>
2024-10-25 21:55:56 -0400

    Order created event

--- order-microservice/Order.Service/Endpoints/OrderApiEndpoint.cs
+++ order-microservice/Order.Service/Endpoints/OrderApiEndpoint.cs
@@ -1,41 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Order.Service.ApiModels;
 using Order.Service.Infrastructure.Data;
+using Order.Service.Infrastructure.EventBus.Abstractions;
+using Order.Service.IntegrationEvents.Events;
 
-namespace Order.Service.Endpoints
+namespace Order.Service.Endpoints;
+
+public static class OrderApiEndpoint
 {
-    public static class OrderApiEndpoint
+    public static void RegisterEndpoints(this IEndpointRouteBuilder routeBuilder)
     {
-        public static void RegisterEndpoints(this IEndpointRouteBuilder routeBuilder)
+        routeBuilder.MapPost("/{customerId}",
+        (IOrderStore orderStore,
+         IEventBus eventBus,
+         string customerId,
+         CreateOrderRequest request) =>
         {
-            routeBuilder.MapPost("/{customerId}",
-            (IOrderStore orderStore,
-             string customerId,
-             CreateOrderRequest request) =>
+            var order = new Models.Order
             {
-                var order = new Models.Order
-                {
-                    CustomerId = customerId
-                };
-                foreach (var product in request.OrderProducts)
-                {
-                    order.AddOrderProduct(product.ProductId, product.Quantity);
-                }
-                orderStore.CreateOrder(order);
-                return TypedResults.Created($"{order.CustomerId}/{order.OrderId}");
-            });
-
-            routeBuilder.MapGet("/{customerId}/{orderId}", IResult
-            ([FromServices] IOrderStore orderStore,
-             string customerId,
-             string orderId) =>
+                CustomerId = customerId
+            };
+            foreach (var product in request.OrderProducts)
             {
-                var order = orderStore.GetCustomerOrderById(customerId, orderId);
-                return order is null
-                  ? TypedResults.NotFound("Order not found for customer")
-                  : TypedResults.Ok(order);
-            });
+                order.AddOrderProduct(product.ProductId, product.Quantity);
+            }
+            
+            orderStore.CreateOrder(order);
 
-        }
+            eventBus.PublishAsync(new OrderCreatedEvent(customerId));
+
+            return TypedResults.Created($"{order.CustomerId}/{order.OrderId}");
+        });
+
+        routeBuilder.MapGet("/{customerId}/{orderId}", IResult
+        ([FromServices] IOrderStore orderStore,
+         string customerId,
+         string orderId) =>
+        {
+            var order = orderStore.GetCustomerOrderById(customerId, orderId);
+            return order is null
+              ? TypedResults.NotFound("Order not found for customer")
+              : TypedResults.Ok(order);
+        });
+
     }
 }
