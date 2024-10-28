44f6cde5c3a7dc3a58a8b055ed902baa8219c649
NeshGogo <rafaelaguero10@hotmail.com>
2024-10-27 23:35:38 -0400

    Docker support to order.service

--- order-microservice/Order.Service/Infrastructure/RabbitMq/RabbitMqExtensions.cs
+++ order-microservice/Order.Service/Infrastructure/RabbitMq/RabbitMqExtensions.cs
@@ -7,7 +7,7 @@ namespace Order.Service.Infrastructure.RabbitMq
         public static void AddRabbitMqEventBus(this IServiceCollection services, IConfigurationManager configurationManager)
         {
             RabbitMqOptions rabbitMqOptons = new();
-            configurationManager.GetSection(rabbitMqOptons.HostName).Bind(rabbitMqOptons);
+            configurationManager.GetSection(RabbitMqOptions.RabbitMqSecctionName).Bind(rabbitMqOptons);
 
             services.AddSingleton<IRabbitMqConnection>(new RabbitMqConnection(rabbitMqOptons));
             services.AddScoped<IEventBus, RabbitMqEventBus>();
