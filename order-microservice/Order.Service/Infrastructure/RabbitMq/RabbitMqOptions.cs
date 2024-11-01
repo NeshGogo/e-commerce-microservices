7538f8b85b05f33289bea7ff33ca66a64e813ae8
NeshGogo <rafaelaguero10@hotmail.com>
2024-10-31 21:53:42 -0400

    Replacing local implementation of RabbitMq by Ecommerce.Shared local library

--- order-microservice/Order.Service/Infrastructure/RabbitMq/RabbitMqOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Order.Service.Infrastructure.RabbitMq;
-
-public class RabbitMqOptions
-{
-    public const string RabbitMqSecctionName = "RabbitMq";
-    public string HostName { get; set; } = string.Empty;
-}
