2acd3907715b1eb813d67631af6388cbe54c7c7e
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-01 22:41:41 -0400

    Remove HostedService from basket.service

--- basket-microservice/Basket.Service/Program.cs
+++ basket-microservice/Basket.Service/Program.cs
@@ -1,13 +1,16 @@
 using Basket.Service.Endpoints;
 using Basket.Service.Infrastructure.Data;
-using Basket.Service.Infrastructure.RabbitMq;
+using Basket.Service.IntegrationEvents;
+using Basket.Service.IntegrationEvents.Handlers;
+using ECommerce.Shared.Infrastructure.EventBus;
 using ECommerce.Shared.Infrastructure.RabbitMq;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IBasketStore, InMemoryBasketStore>();
-builder.Services.AddRabbitMqEventBus(builder.Configuration);
-builder.Services.AddHostedService<RabbitMqHostedService>();
+builder.Services.AddRabbitMqEventBus(builder.Configuration)
+                .AddRabbitMqSubscriberService(builder.Configuration)
+                .AddEventHandler<OrderCreatedEvent, OrderCreatedEventHandler>();
 
 var app = builder.Build();
 
