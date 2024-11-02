8fcd9e34b3f30f13505799114b1832580eec439a
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-01 21:18:43 -0400

    Generic Event Handling

---
+++ shared-libs/ECommerce.Shared/Infrastructure/EventBus/EventBusHandlerExtesions.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Shared.Infrastructure.EventBus.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Shared.Infrastructure.EventBus;
+
+public static class EventBusHandlerExtesions
+{
+    public static IServiceCollection AddEventHandler<TEvent, THandler>(this IServiceCollection services)
+        where TEvent : Event 
+        where THandler : class, IEventHandler<TEvent>
+    {
+        services.AddKeyedTransient<IEventHandler, THandler>(typeof(TEvent));
+
+        services.Configure<EventHandlerRegistration>(o =>
+        {
+            o.EventTypes[typeof(TEvent).Name] = typeof(TEvent);
+        });
+
+        return services;
+    }
+}
