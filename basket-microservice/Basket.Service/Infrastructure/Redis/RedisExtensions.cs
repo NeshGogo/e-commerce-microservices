844f3a2bf67e230f7c69a23ebc36c44cc5dbe5c4
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-04 12:22:06 -0400

    Adding Redis cache to basket service

---
+++ basket-microservice/Basket.Service/Infrastructure/Redis/RedisExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Service.Infrastructure.Redis;
+
+public static class RedisExtensions
+{
+    public static void AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+    {
+        RedisOptions redisOptions = new();
+        configuration.GetRequiredSection(RedisOptions.RedisSectionName).Bind(redisOptions);
+
+        services.AddStackExchangeRedisCache(opt => opt.Configuration = redisOptions.Configuration);
+    }
+}
--- basket-microservice/Basket.Service/Program.cs
+++ basket-microservice/Basket.Service/Program.cs
@@ -1,5 +1,6 @@
 using Basket.Service.Endpoints;
 using Basket.Service.Infrastructure.Data;
+using Basket.Service.Infrastructure.Redis;
 using Basket.Service.IntegrationEvents;
 using Basket.Service.IntegrationEvents.Handlers;
 using ECommerce.Shared.Infrastructure.EventBus;
@@ -12,6 +13,8 @@ builder.Services.AddRabbitMqEventBus(builder.Configuration)
                 .AddRabbitMqSubscriberService(builder.Configuration)
                 .AddEventHandler<OrderCreatedEvent, OrderCreatedEventHandler>()
                 .AddEventHandler<ProductPriceUpdatedEvent, ProductPriceUpdatedEventHandler>();
+
+builder.Services.AddRedisCache(builder.Configuration);
 
 var app = builder.Build();
 
