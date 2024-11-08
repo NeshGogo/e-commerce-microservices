66ac3e46f8367a226060eb4ee64d0785a13696b2
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-07 22:03:22 -0400

    Testing domain models at basket microservice

---
+++ basket-microservice/Basket.Test/Domains/CustomerBasketTest.cs
@@ -0,0 +1,33 @@
+﻿using Basket.Service.Models;
+
+namespace Basket.Test.Domains;
+
+public class CustomerBasketTest
+{
+    [Fact]
+    public void GivenAnEmptyCustomerBasket_WhenCallingAddBasketProduct_ThenProductAddedToBasket()
+    {
+        var product = new BasketProduct("1", "Test Name", 9.99M);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+
+        customerBasket.AddBasketProduct(product);
+
+        Assert.Contains(product, customerBasket.Products);
+    }
+
+    [Fact]
+    public void GivenCustomerBasketWithProduct_WhenCallingAddBasketProductWithExistingProduct_ThenBasketUpdated()
+    {
+        var product = new BasketProduct("1", "Test Name", 9.99M);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+        customerBasket.AddBasketProduct(product);
+        var updatedProduct = product with
+        {
+            Quantity = 2
+        };
+
+        customerBasket.AddBasketProduct(updatedProduct);
+        Assert.Contains(updatedProduct, customerBasket.Products);
+        Assert.Equal(updatedProduct.Quantity, customerBasket.Products.First().Quantity);
+    }
+}
