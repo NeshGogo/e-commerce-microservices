06daa22e72eaa89b00d216f570ae22d993e500bb
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-10 10:44:40 -0400

    Bootstrap Application With WebApplicationFactory at Order.Service

---
+++ order-microservice/Order.Tests/OrderWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Order.Tests;
+
+public class OrderWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(configurationBuilder =>
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.Tests.json")
+                .Build();
+
+            configurationBuilder.AddConfiguration(configuration);
+        });
+
+        return base.CreateHost(builder);
+    }
+}
