4e7ebd4df097791398b879421af690eea3b9141e
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-10 09:59:49 -0400

    Endpoints unit test

--- basket-microservice/Basket.Test/Endpoints/BasketApiEndpointTests.cs
+++ basket-microservice/Basket.Test/Endpoints/BasketApiEndpointTests.cs
@@ -52,4 +52,58 @@ public class BasketApiEndpointTests
         var createdResult = (Created)result;
         Assert.NotNull(createdResult);
     }
+
+    [Fact]
+    public async Task GivenExistingBasketRequest_WhenCallingAddBasketProduct_ThenReturnsNoContentResult()
+    {
+        // Arrange
+        const string customerId = "1";
+        const string productId = "1";
+        var customerBasket = new CustomerBasket { CustomerId = customerId };
+        _basketStore.GetBasketByCustomerIdAsync(customerId)
+            .Returns(customerBasket);
+        var addBasketProductRequest = new AddBasketProductRequest(productId, "Test Name", 1);
+        _cache.GetAsync(productId)
+            .Returns(Encoding.UTF8.GetBytes("1.00"));
+        // Act
+        var result = await BasketApiEndpoints.AddBasketProduct(_basketStore, _cache,
+            customerId, addBasketProductRequest);
+        // Assert
+        Assert.NotNull(result);
+        var noContentResult = (NoContent)result;
+        Assert.NotNull(noContentResult);
+    }
+
+    [Fact]
+    public async Task GivenExistingBasketRequest_WhenCallingDeleteBasketProduct_ThenReturnsNoContentResult()
+    {
+        // Arrange
+        const string customerId = "1";
+        const string productId = "1";
+        var customerBasket = new CustomerBasket { CustomerId = customerId };
+        _basketStore.GetBasketByCustomerIdAsync(customerId)
+            .Returns(customerBasket);
+
+        // Act
+        var result = await BasketApiEndpoints.DeleteBasketProduct(_basketStore, customerId,
+            productId);
+        // Assert
+        Assert.NotNull(result);
+        var noContentResult = (NoContent)result;
+        Assert.NotNull(noContentResult);
+    }
+
+    [Fact]
+    public async Task GivenExistingBasketRequest_WhenCallingDeleteBasket_ThenReturnsNoContentResult()
+    {
+        // Arrange
+        const string customerId = "1";
+
+        // Act
+        var result = await BasketApiEndpoints.DeleteBasket(_basketStore, customerId);
+        // Assert
+        Assert.NotNull(result);
+        var noContentResult = (NoContent)result;
+        Assert.NotNull(noContentResult);
+    }
 }
