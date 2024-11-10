74b54aed727e424ad45bc8275ab3855650640d8f
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-10 11:00:56 -0400

    Scaffold test database

--- order-microservice/Order.Tests/OrderWebApplicationFactory.cs
+++ order-microservice/Order.Tests/OrderWebApplicationFactory.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Order.Service.Infrastructure.Data.EntityFramework;
 
 namespace Order.Tests;
 
-public class OrderWebApplicationFactory : WebApplicationFactory<Program>
+public class OrderWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
+    private OrderContext _orderContext;
+
     protected override IHost CreateHost(IHostBuilder builder)
     {
         builder.ConfigureHostConfiguration(configurationBuilder =>
@@ -19,5 +26,31 @@ public class OrderWebApplicationFactory : WebApplicationFactory<Program>
         });
 
         return base.CreateHost(builder);
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(ApplyMigrations);
+    }
+
+    private void ApplyMigrations(IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+        var scope = serviceProvider.CreateScope();
+        _orderContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+        
+        _orderContext.Database.Migrate();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        if(_orderContext is not null)
+        {
+            return _orderContext.Database.EnsureDeletedAsync();
+        }
+
+        return Task.CompletedTask;
     }
 }
