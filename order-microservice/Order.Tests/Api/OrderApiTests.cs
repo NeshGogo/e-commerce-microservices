bc0a9515d9602dbf3cb39deda61b4e1593f0c7c5
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-13 20:06:42 -0400

    Test when no order exists

---
+++ order-microservice/Order.Tests/Api/OrderApiTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Order.Tests.Api;
+
+public class OrderApiTests : IntegrationTestBase
+{
+    public OrderApiTests(OrderWebApplicationFactory webApplicationFactory) 
+        : base(webApplicationFactory)
+    {
+    }
+
+    [Fact]
+    public async Task GetOrder_WhenNoOrderExists_ThenReturnsNotFount()
+    {
+        var response = await HttpClient.GetAsync($"/1/{Guid.NewGuid()}");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
---
+++ order-microservice/Order.Tests/IntegrationTestBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Order.Service.Infrastructure.Data.EntityFramework;
+
+namespace Order.Tests;
+
+public class IntegrationTestBase : IClassFixture<OrderWebApplicationFactory>
+{
+    public IntegrationTestBase(OrderWebApplicationFactory webApplicationFactory)
+    {
+        var scope = webApplicationFactory.Services.CreateScope();
+
+        OrderContext = scope.ServiceProvider.GetService<OrderContext>();
+        HttpClient = webApplicationFactory.CreateClient();
+        
+    }
+
+    internal readonly OrderContext? OrderContext;
+    internal readonly HttpClient HttpClient;
+}
