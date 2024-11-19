3b4d65bb80c3305cc962fe16cfdc175de4231ccb
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-18 23:21:32 -0400

    Adding OpenTelemetry to Order service

--- order-microservice/Order.Service/Program.cs
+++ order-microservice/Order.Service/Program.cs
@@ -1,4 +1,5 @@
 using ECommerce.Shared.Infrastructure.RabbitMq;
+using ECommerce.Shared.Observability;
 using Order.Service.Endpoints;
 using Order.Service.Infrastructure.Data.EntityFramework;
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServerDatastore(builder.Configuration);
 builder.Services.AddRabbitMqEventBus(builder.Configuration);
 builder.Services.AddRabbitMqEventPublisher();
+builder.Services.AddOpenTelemetryTracing("Order", builder.Configuration, 
+    (tracing) => tracing.WithSqlInstrumentation());
 
 var app = builder.Build();
 
