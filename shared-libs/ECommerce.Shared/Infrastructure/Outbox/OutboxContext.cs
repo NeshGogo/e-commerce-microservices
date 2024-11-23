1f390ef77abce9b59f1af2b0a09d5993860bf64f
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-22 22:45:46 -0400

    Adding outbox dbContext

---
+++ shared-libs/ECommerce.Shared/Infrastructure/Outbox/OutboxContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Shared.Infrastructure.Outbox;
+
+public class OutboxContext : DbContext
+{
+    public OutboxContext(DbContextOptions<OutboxContext> options)
+        : base(options)
+    {        
+    }
+
+    public DbSet<OutBoxEvent> OutBoxEvents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new OutboxEventConfiguration());
+    }
+}
---
+++ shared-libs/ECommerce.Shared/Infrastructure/Outbox/OutboxEventConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Shared.Infrastructure.Outbox
+{
+    public class OutboxEventConfiguration : IEntityTypeConfiguration<OutBoxEvent>
+    {
+        public void Configure(EntityTypeBuilder<OutBoxEvent> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.EventType).IsRequired();
+            builder.Property(o => o.Data).IsRequired();
+        }
+    }
+}
