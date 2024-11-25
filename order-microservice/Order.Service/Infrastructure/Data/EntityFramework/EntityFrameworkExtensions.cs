82c164c998be4556d976f853f8978a51cef1c9a6
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-24 20:38:19 -0400

    sql resilient for Order microservice

--- order-microservice/Order.Service/Infrastructure/Data/EntityFramework/EntityFrameworkExtensions.cs
+++ order-microservice/Order.Service/Infrastructure/Data/EntityFramework/EntityFrameworkExtensions.cs
@@ -7,7 +7,14 @@ public static class EntityFrameworkExtensions
     public static void AddSqlServerDatastore(this IServiceCollection services, IConfigurationManager configuration)
     {
         var connectionString = configuration.GetConnectionString("Default");
-        services.AddDbContext<OrderContext>(opt => opt.UseSqlServer(connectionString));
+        services.AddDbContext<OrderContext>(opt => opt.UseSqlServer(connectionString, options =>
+        {
+            options.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(40),
+                errorNumbersToAdd: [0]);
+                
+        }));
 
         services.AddScoped<IOrderStore, OrderContext>();
     }
