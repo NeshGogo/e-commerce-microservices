f6338e5595665ae16be7c4ab87a31774bcb6ec91
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-26 20:45:47 -0400

    Generate JWTs

--- auth-microservice/Auth.Service/Program.cs
+++ auth-microservice/Auth.Service/Program.cs
@@ -1,8 +1,10 @@
 using Auth.Service.Infrastructure.Data.EntityFramework;
+using Auth.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlServerDataStore(builder.Configuration);
+builder.Services.RegisterTokenService(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ auth-microservice/Auth.Service/Services/JwtTokenService.cs
@@ -0,0 +1,46 @@
+﻿using Auth.Service.Infrastructure.Data;
+using Auth.Service.Models;
+using ECommerce.Shared.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auth.Service.Services;
+
+public class JwtTokenService : ITokenService
+{
+    private readonly IAuthStore _authStore;
+    private readonly string _issuer;
+
+    public JwtTokenService(IAuthStore authStore, AuthOptions options)
+    {
+        _authStore = authStore;
+        _issuer = options.AuthMicroserviceBaseAddress;
+    }
+
+    public async Task<AuthToken?> GenerateAuthenticationToken(string userName, string password)
+    {
+        var user = await _authStore.VerifyUserLogin(userName, password);
+
+        if (user == null) return null;
+
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AuthenticationExtensions.SecurityKey));
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+        var expirationTimeStamp = DateTime.Now.AddMinutes(5);
+
+        List<Claim> claims = [
+            new Claim(JwtRegisteredClaimNames.Name, userName)
+        ];
+
+        JwtSecurityToken tokenOptions = new(
+            issuer: _issuer,
+            claims: claims,
+            expires: expirationTimeStamp,
+            signingCredentials: signingCredentials);
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+        return new AuthToken(tokenString, (int) expirationTimeStamp.Subtract(DateTime.Now).TotalSeconds);
+    }
+}
