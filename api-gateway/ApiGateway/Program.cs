f22c0486e7e6084cb4bd3aeb18dc5ec040e0f4c3
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-27 23:14:33 -0400

    Api gatway authentication and authorization

--- api-gateway/ApiGateway/Program.cs
+++ api-gateway/ApiGateway/Program.cs
@@ -1,14 +1,17 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using ECommerce.Shared.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", false, false);
 
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseJwtAuthentication();
 
 await app.UseOcelot();
 
