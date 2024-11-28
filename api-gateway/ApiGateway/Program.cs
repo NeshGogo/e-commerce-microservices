a783fd74d478c95389845e99cbbfe199bb623ad7
NeshGogo <rafaelaguero10@hotmail.com>
2024-11-27 22:41:11 -0400

    Configuring Ocelot

--- api-gateway/ApiGateway/Program.cs
+++ api-gateway/ApiGateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", false, false);
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
